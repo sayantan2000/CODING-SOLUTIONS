73d6a1da45349b5481a8c1a6b0fdb115e29ec600
sayantan sadhukhan <sayantansk13@gmail.com>
2022-02-18 10:55:13 +0530

    binary tree functon changes

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -37,26 +37,45 @@ void inorder(node *root){
 }
 
 void levelOrderTraversal(node *root){
-	//binarytree level order traversal using queue
-	queue<int> q;
-	q.push(root->data);
+	//level order traversal
+	queue<node*> q;
+	q.push(root);
 	while(!q.empty()){
-		int data=q.front();
-		cout<<data<<" ,"<<" ";
+		node *temp=q.front();
 		q.pop();
-		if(root->left!=NULL) q.push(root->left->data);
-		if(root->right!=NULL) q.push(root->right->data);
+		cout<<temp->data<<" ,"<<" ";
+		if(temp->left!=NULL) q.push(temp->left);
+		if(temp->right!=NULL) q.push(temp->right);
 	}
-	cout<<endl;
+	
+}
+int height(node *root){
+	if(root==NULL) return 0;
+	int le=height(root->left);
+	int ri=height(root->right);
+	return max(le,ri)+1;
+}
+node * inverTree(node *root){
+	node *right=NULL;
+	node *left=NULL;
+	if(root==NULL) return NULL;
+	left=inverTree(root->left);
+	right=inverTree(root->right);
+	root->left=right;
+	root->right=left;
+	return root;
 }
 
 int main(void){
 	node *root=NULL;
 	root=BuildTree(root); // 5 4 8 -1 -1 6 4 -1 5 -1 -1 -1 -1
 	cout<<endl;
+	inverTree(root);
 	cout<<"inorder result ->"<<endl;
 	inorder(root);
 	cout<<endl;
+	cout<<"height of tree ->"<<endl;
+	cout<<height(root);
 	cout<<"level order traversal ->"<<endl;
 	levelOrderTraversal(root);
 	return 0;
