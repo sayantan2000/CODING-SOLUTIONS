fa51fd74a8e4ecfecf2152e730a0ea8a9bb8a914
sayantan sadhukhan <sayantansk13@gmail.com>
2022-02-19 11:59:47 +0530

    changes

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include<queue>
+#include<vector>
 
 using namespace std;
 
@@ -49,12 +50,14 @@ void levelOrderTraversal(node *root){
 	}
 	
 }
+
 int height(node *root){
 	if(root==NULL) return 0;
 	int le=height(root->left);
 	int ri=height(root->right);
 	return max(le,ri)+1;
 }
+
 node * inverTree(node *root){
 	node *right=NULL;
 	node *left=NULL;
@@ -66,17 +69,71 @@ node * inverTree(node *root){
 	return root;
 }
 
+bool isPresent(node *root,int ele){
+	if(root==NULL) return false;
+	if(root->data==ele) return true;
+	isPresent(root->left,ele);
+	isPresent(root->right,ele);
+}
+
+void leftView(vector<int> &v,node *root,int lvl){
+	if(root==NULL) return ;
+	if(lvl==v.size())
+	   v.push_back(root->data);
+	leftView(v,root->left,lvl+1);
+	leftView(v,root->right,lvl+1);
+
+}
+
+void righttView(vector<int> &v,node *root,int lvl){
+	if(root==NULL) return ;
+	if(lvl==v.size())
+	   v.push_back(root->data);
+	leftView(v,root->right,lvl+1);
+	leftView(v,root->left,lvl+1);
+	
+}
+
 int main(void){
+
+	/*//sample binary tree
+	*   4
+	* 3   5
+	7  2     6
+	            9
+	*
+	*/
+
+
 	node *root=NULL;
-	root=BuildTree(root); // 5 4 8 -1 -1 6 4 -1 5 -1 -1 -1 -1
+	vector<int> v; 
+	vector<int> ri;
+	root=BuildTree(root); // creation of tree
+	cout<<endl; //printing new line
+	leftView(v,root,0); //stores the left view of the Binary tree in the vectot v;
+	for(auto i:v){  //prints the vector
+		cout<<i<<" ";
+	}
 	cout<<endl;
-	inverTree(root);
+	cout<<"<---------------separator------------------->"<<endl; ///line separator
+	righttView(ri,root,0); ////stores the right view of the Binary tree in the vectot ri
+	for(auto i:ri){  ///prints the  vector
+		cout<<i<<" ";
+	}
+	/*inverTree(root);				 //swaps the data of child nodes from left to right;
 	cout<<"inorder result ->"<<endl;
-	inorder(root);
+	inorder(root); 		//inorder traversal	
 	cout<<endl;
 	cout<<"height of tree ->"<<endl;
-	cout<<height(root);
+	cout<<height(root);  		//returs the height of binary treee
 	cout<<"level order traversal ->"<<endl;
-	levelOrderTraversal(root);
+	levelOrderTraversal(root);    ///Level Order Traversal of Binary tree 
+	bool res=isPresent(root,5);  ///search operation in binary tree \\return trur if element found else false
+	if(res)
+	   cout<<"The Element is present" <<"  "<<res ;
+	else 
+	  cout<<"Noop!";*/
+	delete root;
+
 	return 0;
 }
